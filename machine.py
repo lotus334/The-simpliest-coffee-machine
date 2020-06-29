f486469e3d5c2fc46ac93747836f94a63c1199ba
Dmitriy <lotus33497@yandex.ru>
2020-06-29 09:51:19 +0700

    Update machine.py
    Add some more functions.
    Now the machine can write which resource has run out. It works endlessly to make coffee for all interested persons until the shutdown signal is given. The program can output all the resources that has when the user types 'remaining'.

--- machine.py
+++ machine.py
@@ -3,76 +3,97 @@ avail_milk = 540
 avail_coffee_beans = 120
 avail_disposable = 9
 avail_money = 550
+avail = [400, 540, 120, 9, 550]
+ingr = ['water', 'milk', 'coffee beans', 'disposable cups', 'money']
 
 def machine_has():
     print('The coffee machine has:')
-    print(str(avail_water) + ' of water')
-    print(str(avail_milk) + ' of milk')
-    print(str(avail_coffee_beans) + ' of coffee beans')
-    print(str(avail_disposable) + ' of disposable cups')
-    print(str(avail_money) + ' of money')
+    print(str(avail[0]) + ' of water')
+    print(str(avail[1]) + ' of milk')
+    print(str(avail[2]) + ' of coffee beans')
+    print(str(avail[3]) + ' of disposable cups')
+    print(str(avail[4]) + ' of money')
+
+def compare_amount(coffee):
+    for index in range(len(avail) - 1):
+        if avail[index] < coffee[index]:
+            global ingridient
+            ingridient = ingr[index]
+            return False
+    return True
 
 def take_coffee():
-    global avail_water
-    global avail_milk
-    global avail_coffee_beans
-    global avail_disposable
-    global avail_money
-    print('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:')
-    type_of_coffee = int(input())
-    if type_of_coffee == 1:
+    global avail
+    print('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:')
+    type_of_coffee = input()
+    espresso = [250, 0, 16, 1, 4]
+    latte = [350, 75, 20, 1, 7]
+    cappuccino = [200, 100, 12, 1, 6]
+    if type_of_coffee == '1':
         #Espresso
-        avail_water -= 250
-        avail_coffee_beans -= 16
-        avail_disposable -= 1
-        avail_money += 4
-    elif type_of_coffee == 2:
+        if compare_amount(espresso) == True:
+            print('I have enough resources, making you a coffee!')
+            avail[0] -= 250
+            avail[1] -= 0
+            avail[2] -= 16
+            avail[3] -= 1
+            avail[4] += 4
+        else:
+            print('Sorry, not enought ' + ingridient + '!')
+    elif type_of_coffee == '2':
         #Latte
-        avail_water -= 350
-        avail_milk -= 75
-        avail_coffee_beans -= 20
-        avail_disposable -= 1
-        avail_money += 7
-    elif type_of_coffee == 3:
+        if compare_amount(latte) == True:
+            print('I have enough resources, making you a coffee!')
+            avail[0] -= 350
+            avail[1] -= 75
+            avail[2] -= 20
+            avail[3] -= 1
+            avail[4] += 7
+        else:
+            print('Sorry, not enought ' + ingridient + '!')
+    elif type_of_coffee == '3':
         #Cappuccino
-        avail_water -= 200
-        avail_milk -= 100
-        avail_coffee_beans -= 12
-        avail_disposable -= 1
-        avail_money += 6
+        if compare_amount(cappuccino) == True:
+            print('I have enough resources, making you a coffee!')
+            avail[0] -= 200
+            avail[1] -= 100
+            avail[2] -= 12
+            avail[3] -= 1
+            avail[4] += 6
+        else:
+            print('Sorry, not enought ' + ingridient + '!')
+    elif type_of_coffee == 'back':
+        None
         
 def fill_machine():
-    global avail_water
-    global avail_milk
-    global avail_coffee_beans
-    global avail_disposable
+    global avail
     print('Write how many ml of water do you want to add:')
-    avail_water += int(input())
+    avail[0] += int(input())
     print('Write how many ml of milk do you want to add:')
-    avail_milk += int(input())
+    avail[1] += int(input())
     print('Write how many grams of coffee beans do you want to add:')
-    avail_coffee_beans += int(input())
+    avail[2] += int(input())
     print('Write how many disposable cups of coffee do you want to add:')
-    avail_disposable += int(input())
+    avail[3] += int(input())
 
 def action():
-    machine_has()
-    print()
-    print('Write action (buy, fill, take):')
-    action = input()
-    if action == 'buy':
-        take_coffee()
-    elif action == 'fill':
-        fill_machine()
-    elif action == 'take':
-        global avail_money
-        print('I gave you $' + str(avail_money))
-        avail_money = 0
-    print()
-    machine_has()
+    action = ''
+    while action != 'exit':
+        print('Write action (buy, fill, take, remaining, exit):')
+        action = input()
+        if action == 'buy':
+            take_coffee()
+        elif action == 'fill':
+            fill_machine()
+        elif action == 'take':
+            global avail
+            print('I gave you $' + str(avail_money))
+            avail[4] = 0
+        elif action == 'remaining':
+            machine_has()
+        print()
 
 action()
-
 '''
 print('Write how many ml of water the coffee machine has:')
 avail_water = int(input())
