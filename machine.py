9a29cd377ddead4c6f50adb568099300efe01104
Dmitriy <lotus33497@yandex.ru>
2020-06-29 10:14:48 +0700

    Update machine.py
    Delete old variables

--- machine.py
+++ machine.py
@@ -1,8 +1,3 @@
-avail_water = 400
-avail_milk = 540
-avail_coffee_beans = 120
-avail_disposable = 9
-avail_money = 550
 avail = [400, 540, 120, 9, 550]
 ingr = ['water', 'milk', 'coffee beans', 'disposable cups', 'money']
 
@@ -94,34 +89,3 @@ def action():
         print()
 
 action()
-'''
-print('Write how many ml of water the coffee machine has:')
-avail_water = int(input())
-print('Write how many ml of milk the coffee machine has:')
-avail_milk = int(input())
-print('Write how many grams of coffee beans the coffee machine has:')
-avail_coffee_beans = int(input())
-print('Write how many cups of coffee you will need:')
-num_of_cups = int(input())
-#print('For ' + str(num_of_cups) + ' cups of coffee you will need:')
-req_water = num_of_cups * 200
-req_milk = num_of_cups * 50
-req_coffee_beans = num_of_cups * 15
-#print(str(req_water) + ' ml of water')
-#print(str(req_milk) + ' ml of milk')
-#print(str(req_coffee_beans) + ' g of coffee beans')
-avail_cups = 0
-if avail_water / 200 > 0:
-    avail_cups = int(avail_water / 200)
-if avail_milk / 50 < avail_cups:
-    avail_cups = int(avail_milk / 50)
-if avail_coffee_beans / 15 < avail_cups:
-    avail_cups = int(avail_coffee_beans / 15)
-if avail_cups >= num_of_cups:
-    print('Yes, I can make that amount of coffee', end = '')
-    left_cups = avail_cups - num_of_cups
-    if left_cups > 0:
-        print(' (and even ' + str(int(left_cups)) + ' more than that)')
-else:
-    print('No, I can make only ' + str(avail_cups) + ' cups of coffee')
-'''
